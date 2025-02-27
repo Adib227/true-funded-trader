66dbd27837f46ac58a217505eb4d6f5213c08ba4
Iftekhar Aziz Adib <iftekharadib227@gmail.com>
2025-02-28 01:12:50 +0600

    finished the funding programme section

--- src/Pages/Fundingprogramme/Fundingprogramme.jsx
+++ src/Pages/Fundingprogramme/Fundingprogramme.jsx
@@ -2,35 +2,159 @@ const Fundingprogramme = () => {
   return (
     <div
       style={{ fontFamily: 'Montserrat, sans-serif' }}
-      className="bg-[#05133B] text-white flex flex-row lg:flex-col"
+      className="bg-[#05133B] text-white flex flex-row lg:flex-col h-screen lg:h-[880px]"
     >
       <div className="text-center pt-14 pb-20">
-        <span className="text-5xl font-extrabold"> Why Choose </span>
+        <span className="text-5xl font-extrabold bg-gradient-to-r from-[#FF2E00] to-[#FFF500]   bg-clip-text text-transparent">
+          {' '}
+          Why Choose{' '}
+        </span>
         <span className="text-5xl font-normal">
           {' '}
           Our <br /> Funding Program?
         </span>
       </div>
-      <div className="flex flex-col lg:flex-row w-[1450px] mx-auto justify-between ">
+      <div className="flex flex-col lg:flex-row w-[1500px] mx-auto justify-between ">
         {/* left side starts here */}
 
         <h1>
-          <p>
+          <p className="space-y-5">
             {' '}
             <div className="relative z-10 flex items-center space-x-4">
-              <div className="px-6 py-3 ml-20 text-center  bg-[#151718] text-white font-bold text-xl rounded-lg shadow-xl w-96 border-1 border-white ">
-                <span className="text-[14px] font-normal ">
+              <div className="px-6 py-3  text-left  bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal">
                   Receive 95% Profit share
                 </span>{' '}
                 {/* <span className="text-6xl block font-medium">Payout</span> */}
               </div>
-              <div className="w-40 h-40 p-5 -ml-20 items-center justify-center rounded-full bg-white border-[12px] border-purple-500 shadow-xl ">
-                <span className="text-6xl font-bold text-purple-700 text-center ">
-                  24
-                  <br />
+              <div className="w-20 h-20 p-1.5 -ml-11 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/rKTNT9My/Icon-for-why-choose.png"
+                    alt=""
+                  />
                 </span>
-                <span className="text-3xl  font-bold text-purple-700 text-center pb-6">
-                  Hours
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="px-6 py-3 text-left  bg-[#151718] text-white  rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal ">
+                  First Payout on demand then biweekly payouts
+                </span>{' '}
+              </div>
+              <div className="w-20 h-20 p-1.5 -ml-11 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/5gmQXqQ5/Icon-for-why-choose-1.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="px-6 py-3  text-left leading-4.5   bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal">
+                  1 step evaluation, 2 step evaluation and instant funding all
+                  available
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+              <div className="w-20 h-20 p-1.5 -ml-11 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/zhQ8LCCx/Icon-for-why-choose-2.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="px-6 py-3  text-left  bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal">
+                  Scaling Daily and maximum loss limits
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+              <div className="w-20 h-20 p-1.5 -ml-11 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/JjBrYTRm/Icon-for-why-choose-3.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="px-6 py-3  text-left  bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal">
+                  20% profit share from challenge phases
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+              <div className="w-20 h-20 p-1.5 -ml-11 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/tRJBqsR/Icon-for-why-choose-4.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="px-6 py-3  text-left   bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal">
+                  Trade your own daD-to-day Strategy or system
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+              <div className="w-20 h-20 p-1.5 -ml-11 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/GSFjfLn/Icon-for-why-choose-5.png"
+                    alt=""
+                  />
                 </span>
               </div>
             </div>
@@ -40,17 +164,17 @@ const Fundingprogramme = () => {
         {/* left side ends here */}
 
         {/* image start */}
-        <h1>
+        <div className="grid items-center">
           <img src="https://i.ibb.co.com/XxD2hq3F/Group.png" alt="" />
-        </h1>
+        </div>
         {/* image end */}
 
         {/* right side start here */}
 
         <h1>
-          <p>
+          <p className="space-y-5">
             {' '}
-            <div className="relative z-10 flex items-center space-x-4">
+            {/* <div className="relative z-10 flex items-center space-x-4">
               <div
                 style={{ fontFamily: 'Montserrat, sans-serif' }}
                 className="w-40 h-40   absolute  grid grid-cols-1 items-center justify-center rounded-full bg-white border-[12px] border-purple-500 shadow-xl "
@@ -63,12 +187,147 @@ const Fundingprogramme = () => {
                   Hours
                 </span>
               </div>
-              <div
-                style={{ fontFamily: 'Anton, sans-serif' }}
-                className="px-6 py-3 ml-20 text-center   bg-gradient-to-b from-[#6f28ff] to-[#b827ff] text-white font-bold text-xl rounded-lg shadow-xl w-96 border-4 border-[#DB00FF] border-b-[#0000001A]"
-              >
+              <div className="px-6 py-3 ml-20 text-center   bg-gradient-to-b from-[#6f28ff] to-[#b827ff] text-white font-bold text-xl rounded-lg shadow-xl w-96 border-4 border-[#DB00FF] border-b-[#0000001A]">
                 <span className="text-4xl font-normal">Guaranteed</span>{' '}
                 <span className="text-6xl block font-medium">Payout</span>
+              </div>
+            </div> */}
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="w-20 h-20 p-1.5 -mr-6 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/6JfNgBj3/Icon-for-why-choose-6.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+              <div className="px-6 py-3 ml-1  leading-5  bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal ml-3">
+                  EA's and News trading allow
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="w-20 h-20 p-1.5 -mr-6 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/GSFjfLn/Icon-for-why-choose-5.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+              <div className="px-6 py-3 ml-1  leading-5  bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal ml-3">
+                  Hold trades over the weekend
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="w-20 h-20 p-1.5 -mr-6 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/rKTNT9My/Icon-for-why-choose.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+              <div className="px-6 py-3 ml-1  leading-5  bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal ml-3">
+                  Receive 95% Profit share
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="w-20 h-20 p-1.5 -mr-6 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/5hpPxV9K/Icon-for-why-choose-7.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+              <div className="px-6 py-3 ml-1  leading-4 bg-[#151718] text-white font-bold text-xl rounded-r-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal ml-3 ">
+                  Real-time Data included on dashboard
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="w-20 h-20 p-1.5 -mr-6 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/CDM4gHw/Icon-for-why-choose-8.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+              <div className="px-6 py-3 ml-1  leading-5  bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal ml-3">
+                  Simple risk management rules
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
+              </div>
+            </div>
+            <div className="relative z-10 flex items-center space-x-4">
+              <div className="w-20 h-20 p-1.5 -mr-6 items-center justify-center rounded-full bg-[#151718] border-1  border-transparent ">
+                <span className="absolute  w-20 h-20 ">
+                  {' '}
+                  <img
+                    src="https://i.ibb.co.com/23MKN5kq/Ellipse-50.png"
+                    alt=""
+                  />
+                </span>{' '}
+                <span className="relative  w-16 h-16  shadow-cyan-700 shadow-md  rounded-full flex justify-center items-center ">
+                  <img
+                    src="https://i.ibb.co.com/60jH8PnQ/Icon-for-why-choose-9.png"
+                    alt=""
+                  />
+                </span>
+              </div>
+              <div className="px-6 py-3 ml-1  leading-4  bg-[#151718] text-white font-bold text-xl rounded-xl shadow-xl w-96 border-1 border-white  ">
+                <span className="text-[14px] font-normal ml-3">
+                  Trade with multiple accounts up to $5M
+                </span>{' '}
+                {/* <span className="text-6xl block font-medium">Payout</span> */}
               </div>
             </div>
           </p>
